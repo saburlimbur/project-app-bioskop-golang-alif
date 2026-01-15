e20e69e080e1d2ede3c693ae8e9269012b32d302
Alif Dwi Rahman <alifdwirahman54@gmail.com>
2026-01-15 13:16:25 +0700

    feat(payment): implement payment creation and listing payment methods

--- internal/data/repository/payment.go
+++ internal/data/repository/payment.go
@@ -1,22 +1,135 @@
 package repository
 
 import (
-	"database/sql"
+	"alfdwirhmn/bioskop/internal/data/entity"
+	"alfdwirhmn/bioskop/pkg/database"
+	"context"
 
 	"go.uber.org/zap"
 )
 
 type PaymentRepository interface {
+	CreatePayment(ctx context.Context, py *entity.Payment) (*entity.Payment, error)
+	FindPaymentWithMethod(ctx context.Context, paymentID int) (*entity.Payment, error)
 }
 
 type paymentRepo struct {
-	db  *sql.DB
-	log *zap.Logger
+	DB   database.PgxIface
+	Logg *zap.Logger
 }
 
-func NewPaymentRepository(db *sql.DB, log *zap.Logger) PaymentRepository {
+func NewPaymentRepository(db database.PgxIface, log *zap.Logger) PaymentRepository {
 	return &paymentRepo{
-		db:  db,
-		log: log,
+		DB:   db,
+		Logg: log,
 	}
+}
+
+func (pr *paymentRepo) CreatePayment(ctx context.Context, py *entity.Payment) (*entity.Payment, error) {
+	query := `
+		INSERT INTO payments (booking_id, payment_method_id, amount, payment_status, payment_date, transaction_id)
+		VALUES ($1, $2, $3, $4, $5, $6)
+		RETURNING
+		id, booking_id, payment_method_id, amount, payment_status,
+		payment_date, transaction_id, created_at
+	`
+
+	var payment entity.Payment
+	err := pr.DB.QueryRow(ctx, query,
+		py.BookingID,
+		py.PaymentMethodID,
+		py.Amount,
+		py.PaymentStatus,
+		py.PaymentDate,
+		py.TransactionID,
+	).Scan(
+		&payment.ID,
+		&payment.BookingID,
+		&payment.PaymentMethodID,
+		&payment.Amount,
+		&payment.PaymentStatus,
+		&payment.PaymentDate,
+		&payment.TransactionID,
+		&payment.CreatedAt,
+	)
+
+	if err != nil {
+		pr.Logg.Error("failed to create payment", zap.Error(err))
+		return nil, err
+	}
+
+	pr.Logg.Info("payment created successfully")
+	return &payment, nil
+}
+
+func (pr *paymentRepo) FindPaymentMethodByID(
+	ctx context.Context,
+	id int,
+) (*entity.PaymentMethod, error) {
+
+	query := `
+		SELECT id, name, code, is_active, created_at
+		FROM payment_methods
+		WHERE id = $1
+	`
+
+	var pm entity.PaymentMethod
+	err := pr.DB.QueryRow(ctx, query, id).Scan(
+		&pm.ID,
+		&pm.Name,
+		&pm.Code,
+		&pm.IsActive,
+		&pm.CreatedAt,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &pm, nil
+}
+
+func (pr *paymentRepo) FindPaymentWithMethod(ctx context.Context, id int) (*entity.Payment, error) {
+
+	query := `
+		SELECT
+			p.id,
+			p.booking_id,
+			p.amount,
+			p.payment_status,
+			p.payment_date,
+			p.transaction_id,
+			p.created_at,
+
+			pm.id,
+			pm.name,
+			pm.code
+		FROM payments p
+		JOIN payment_methods pm ON pm.id = p.payment_method_id
+		WHERE p.id = $1
+	`
+
+	var pay entity.Payment
+	var method entity.PaymentMethod
+
+	err := pr.DB.QueryRow(ctx, query, id).Scan(
+		&pay.ID,
+		&pay.BookingID,
+		&pay.Amount,
+		&pay.PaymentStatus,
+		&pay.PaymentDate,
+		&pay.TransactionID,
+		&pay.CreatedAt,
+
+		&method.ID,
+		&method.Name,
+		&method.Code,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	pay.PaymentMethod = &method
+	return &pay, nil
 }
--- internal/usecase/payment.go
+++ internal/usecase/payment.go
@@ -1,1 +1,106 @@
 package usecase
+
+import (
+	"alfdwirhmn/bioskop/internal/data/entity"
+	"alfdwirhmn/bioskop/internal/data/repository"
+	"alfdwirhmn/bioskop/internal/dto"
+	"alfdwirhmn/bioskop/pkg/database"
+	"alfdwirhmn/bioskop/pkg/utils"
+	"context"
+	"errors"
+	"time"
+
+	"github.com/jackc/pgx/v5"
+	"go.uber.org/zap"
+)
+
+type PaymentServiceCase interface {
+	CreatePayment(ctx context.Context, userID int, req dto.CreatePaymentRequest) (*entity.Payment, error)
+	ListPaymentMethods(ctx context.Context) ([]entity.PaymentMethod, error)
+}
+
+type paymentServiceCase struct {
+	Repo      repository.Repository
+	TxManager database.TxManager
+	Logger    *zap.Logger
+	Config    utils.Configuration
+}
+
+func NewPaymentServiceCase(repo repository.Repository, txMgr database.TxManager, log *zap.Logger, conf utils.Configuration) PaymentServiceCase {
+	return &paymentServiceCase{
+		Repo:      repo,
+		TxManager: txMgr,
+		Logger:    log,
+		Config:    conf,
+	}
+}
+
+func (ps *paymentServiceCase) CreatePayment(ctx context.Context, userID int, req dto.CreatePaymentRequest) (*entity.Payment, error) {
+
+	var result *entity.Payment
+
+	err := ps.TxManager.WithTransaction(ctx, func(tx pgx.Tx) error {
+		repoTx := ps.Repo.WithTx(tx)
+
+		booking, err := repoTx.BookingRepo.FindBookingById(ctx, req.BookingID)
+		if err != nil {
+			return err
+		}
+
+		if booking.BookingStatus != "pending" {
+			return errors.New("booking already paid or cancelled")
+		}
+
+		// cek payment method yang ada dan aktif
+		method, err := repoTx.PaymentMethodRepo.FindByID(ctx, req.PaymentMethodID)
+		if err != nil {
+			return err
+		}
+		if !method.IsActive {
+			return errors.New("payment method is inactive")
+		}
+
+		now := time.Now()
+		trxID := utils.GenerateTransactionID()
+
+		payment := &entity.Payment{
+			BookingID:       booking.ID,
+			PaymentMethodID: method.ID,
+			Amount:          booking.TotalPrice,
+			PaymentStatus:   "success",
+			PaymentDate:     &now,
+			TransactionID:   &trxID,
+		}
+
+		created, err := repoTx.PaymentRepo.CreatePayment(ctx, payment)
+		if err != nil {
+			return err
+		}
+
+		// transaction update booking to confirmed
+		err = repoTx.BookingRepo.UpdateBookingStatus(ctx, booking.ID, "confirmed")
+		if err != nil {
+			return err
+		}
+
+		// ambil payment dan method
+		full, err := repoTx.PaymentRepo.FindPaymentWithMethod(ctx, created.ID)
+		if err != nil {
+			return err
+		}
+
+		result = full
+		return nil
+	})
+
+	if err != nil {
+		ps.Logger.Error("create payment failed", zap.Error(err))
+		return nil, err
+	}
+
+	return result, nil
+}
+
+func (ps *paymentServiceCase) ListPaymentMethods(ctx context.Context) ([]entity.PaymentMethod, error) {
+	return ps.Repo.PaymentMethodRepo.ListActive(ctx)
+}
