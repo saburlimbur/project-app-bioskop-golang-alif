47141bf1107cee4d5fe524874403effe813eca3d
Alif Dwi Rahman <alifdwirahman54@gmail.com>
2026-01-15 13:17:23 +0700

    refactor(core): reorganize repository and usecase containers

--- internal/data/repository/repository_container.go
+++ internal/data/repository/repository_container.go
@@ -1,21 +1,53 @@
 package repository
 
 import (
-	"github.com/jackc/pgx/v5/pgxpool"
+	"alfdwirhmn/bioskop/pkg/database"
+
 	"go.uber.org/zap"
 )
 
 type Repository struct {
-	UserRepo   UserRepository
-	CinemaRepo CinemaRepository
+	DB   database.PgxIface
+	Logg *zap.Logger
+
+	UserRepo          UserRepository
+	CinemaRepo        CinemaRepository
+	BookingRepo       BookingRepository
+	ShowtimeRepo      ShowtimeRepository
+	PaymentRepo       PaymentRepository
+	PaymentMethodRepo PaymentMethodRepository
 
 	SessionRepo SessionRepository
 }
 
-func NewRepository(db *pgxpool.Pool, log *zap.Logger) Repository {
+// constructor utama untuk abstract query
+func NewRepository(db database.PgxIface, log *zap.Logger) Repository {
 	return Repository{
-		UserRepo:    NewUserRepository(db, log),
-		SessionRepo: NewSessionRepository(db, log),
-		CinemaRepo:  NewCinemaRepository(db, log),
+		DB:   db,
+		Logg: log,
+
+		UserRepo:          NewUserRepository(db, log),
+		SessionRepo:       NewSessionRepository(db, log),
+		CinemaRepo:        NewCinemaRepository(db, log),
+		ShowtimeRepo:      NewShowtimeRepository(db, log),
+		BookingRepo:       NewBookingRepository(db, log),
+		PaymentRepo:       NewPaymentRepository(db, log),
+		PaymentMethodRepo: NewPaymentMethodRepository(db, log),
+	}
+}
+
+// transaction
+func (r Repository) WithTx(tx database.PgxIface) Repository {
+	return Repository{
+		DB:   tx,
+		Logg: r.Logg,
+
+		UserRepo:          NewUserRepository(tx, r.Logg),
+		SessionRepo:       NewSessionRepository(tx, r.Logg),
+		CinemaRepo:        NewCinemaRepository(tx, r.Logg),
+		ShowtimeRepo:      NewShowtimeRepository(tx, r.Logg),
+		BookingRepo:       NewBookingRepository(tx, r.Logg),
+		PaymentRepo:       NewPaymentRepository(tx, r.Logg),
+		PaymentMethodRepo: NewPaymentMethodRepository(tx, r.Logg),
 	}
 }
--- internal/usecase/usecase_container.go
+++ internal/usecase/usecase_container.go
@@ -2,23 +2,29 @@ package usecase
 
 import (
 	"alfdwirhmn/bioskop/internal/data/repository"
+	"alfdwirhmn/bioskop/pkg/database"
 	"alfdwirhmn/bioskop/pkg/utils"
 
 	"go.uber.org/zap"
 )
 
 type Service struct {
-	UserService   UserServiceCase
-	CinemaService CinemasServiceCase
+	UserService    UserServiceCase
+	CinemaService  CinemasServiceCase
+	BookingService BookingServiceCase
+	PaymentService PaymentServiceCase
 }
 
 func NewService(
 	repo repository.Repository,
+	txMgr database.TxManager,
 	log *zap.Logger,
 	conf utils.Configuration,
 ) Service {
 	return Service{
-		UserService:   NewUserServiceCase(repo, log, conf),
-		CinemaService: NewCinemaServiceCase(repo, log, conf),
+		UserService:    NewUserServiceCase(repo, log, conf),
+		CinemaService:  NewCinemaServiceCase(repo, log, conf),
+		BookingService: NewBookingServiceCase(repo, txMgr, log, conf),
+		PaymentService: NewPaymentServiceCase(repo, txMgr, log, conf),
 	}
 }
